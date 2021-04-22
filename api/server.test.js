3a930f1d6a21e4f4c4f09073439608cf414328db
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-22 18:56:43 -0400

    server test are done and MVP

--- api/server.test.js
+++ api/server.test.js
@@ -17,10 +17,37 @@ afterAll(async ()=>{
 })
 
 describe('server', () => {
-    describe('[GET] /', () => {
+    describe('[GET] /dances', () => {
         it('responds with 200 ok', async () => {
-            const res = await request(server).get('/')
+            const res = await request(server).get('/dances')
             expect(res.status).toBe(200)
+        })
+        it("returns right num o dances", async ()=>{
+            let res 
+            await db("dances").insert(bnm)
+            res = await request(server).get("/dances")
+            expect(res.body).toHaveLength(1)
+
+            await db("dances").insert(hphp)
+            res = await request(server).get("/dances")
+            expect(res.body).toHaveLength(2)
+        })
+        it("returns right format for dances", async ()=>{
+            await db("dances").insert(bnm)
+            await db("dances").insert(hphp)
+            const res = await request(server).get("/dances")
+            expect(res.body[0]).toMatchObject({id:1, ...bnm})
+            expect(res.body[1]).toMatchObject({id:2, ...hphp})
+        })
+    })
+    describe("[POST] /dances", ()=>{
+        it("resopnds with newly created dance", async ()=>{
+            let res 
+            res = await request(server).post("/dances").send(bnm)
+            expect(res.body).toMatchObject({id:1, ...bnm})
+
+            res = await request(server).post("/dances").send(hphp)
+            expect(res.body).toMatchObject({id:2, ...hphp})
         })
     })
 })
